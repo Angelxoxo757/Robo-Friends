ff2389130d17d51e431723a95a11e3f9ddbeba1c
Angelxoxo757 <maria.cromety@gmail.com>
2021-01-01 22:26:03 -0500

    Started RoboFriends App

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img alt='photo' src='' />
+            <div>
+                <h2>Callie Scolari</h2>
+                <p>callie.scolari@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+export default Card;
