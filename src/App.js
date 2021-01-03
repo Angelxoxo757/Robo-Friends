34850eaea3fb1a9ff9798bea275b1f8499a63442
Angelxoxo757 <maria.cromety@gmail.com>
2021-01-03 12:05:26 -0500

    Completed RoboFriends scroll functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import './App.css';
 import Scroll from './Scroll';
+import './App.css';
 
 class App extends Component {
     constructor() {
@@ -37,9 +37,9 @@ class App extends Component {
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
-                    <scroll>
+                    <Scroll>
                         <CardList robots={filteredRobots} />
-                    </scroll>
+                    </Scroll>
                 </div>
             );
         }
